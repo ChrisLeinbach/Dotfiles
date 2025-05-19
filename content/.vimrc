200f5b803ccdf6b514154ea11d8f70afb8a39e34
Christopher Leinbach <christopher.leinbach@gmail.com>
2025-05-19 15:49:47 -0400

    Init with Basic Dotfiles.

---
+++ content/.vimrc
@@ -0,0 +1,3 @@
+colorscheme darkblue
+
+set number
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+script_path=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+git -C $script_path pull origin master
+rsync -avh --no-perms $script_path/content/ ~
+
+source ~/.bash_profile
